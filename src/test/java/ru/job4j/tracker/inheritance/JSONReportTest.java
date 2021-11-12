cbfa6860625b89c9708dbf990c330a6e3ba36068
AlenaAgeeva <alenapvageeva@gmail.com>
2021-11-12 19:33:09 -0400

    4. Переопределение [#174890]

--- src/test/java/ru/job4j/tracker/inheritance/JSONReportTest.java
+++ src/test/java/ru/job4j/tracker/inheritance/JSONReportTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class JSONReportTest {
-
     @Test
     public void whenTestGenerateMethod() {
         String ln = System.lineSeparator();

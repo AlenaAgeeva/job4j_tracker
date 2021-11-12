8793b421a1855425399f3def4fd6824979b3f583
AlenaAgeeva <alenapvageeva@gmail.com>
2021-11-12 17:39:39 -0400

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/test/java/ru/job4j/tracker/pojo/ShopTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker.pojo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShopTest {
+
+    @Test
+    public void whenLastNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        int rsl = Shop.indexOfNull(products);
+        int expected = 3;
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenFirstNull() {
+        Product[] products = new Product[5];
+        products[1] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
+        int expected = 0;
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenHasNotNull() {
+        Product[] products = new Product[1];
+        products[0] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
+        int expected = -1;
+        Assert.assertEquals(expected, rsl);
+    }
+}

284ace501a784db354d8c7c7d08eed1338a306c3
AlenaAgeeva <alenapvageeva@gmail.com>
2021-11-12 12:52:52 -0400

    6. Date. Отображение даты. [#310017 #227817]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter date = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(date.format(item.getCreated()));
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(dateFormatter.format(item.getCreated()));
     }
 }

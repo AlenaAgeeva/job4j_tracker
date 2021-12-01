ea4dad14dde46bc8457325cff2ab8cc2245cd838
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-01 15:15:00 -0400

    1. Телефонный справочник на базе ArrayList [#41598 #234522]

--- src/test/java/ru/job4j/tracker/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/tracker/collection/PhoneDictionaryTest.java
@@ -17,4 +17,17 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertEquals(persons.get(0).getSurname(), "Arsentev");
     }
+
+    @Test
+    public void whenReturnEmptyList() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Semen", "Semenov", "765872", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("Igor");
+        assertEquals(persons.size(), 0);
+    }
 }

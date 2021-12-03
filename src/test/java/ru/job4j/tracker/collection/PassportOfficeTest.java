33d4a30995bcc646557ee72edbbb0ffa2c797820
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-03 11:05:19 -0400

    1. Паспорт и жители [#212656 #235209]

--- src/test/java/ru/job4j/tracker/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/tracker/collection/PassportOfficeTest.java
@@ -11,5 +11,22 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         Assert.assertEquals(office.get(citizen.getPassport()), citizen);
+        Assert.assertEquals(office.getSize(), 1);
+    }
+
+    @Test
+    public void addDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen1 = new Citizen("2f44a", "Petr Ar");
+        Citizen citizen2 = new Citizen("2f44a", "Artem A");
+        Citizen citizen3 = new Citizen("2f44a", "Igor I");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen1);
+        office.add(citizen2);
+        office.add(citizen1);
+        office.add(citizen3);
+        Assert.assertFalse(office.add(citizen1));
+        Assert.assertEquals(office.getSize(), 1);
     }
 }

06eb436cf1d935dd3bba15fcdf0b11e254ecd1cf
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-04 16:11:05 -0400

    0. Сортировка [#10096 #235656]

--- src/test/java/ru/job4j/tracker/collection/ItemSortTest.java
+++ src/test/java/ru/job4j/tracker/collection/ItemSortTest.java
@@ -14,39 +14,21 @@ public class ItemSortTest {
     public void sortDesc() {
         List<Item> list = new ArrayList<>() {
             {
-                Item item = new Item();
-                Item item1 = new Item();
-                Item item2 = new Item();
-                Item item3 = new Item();
-                item.setName("a");
-                item1.setName("j");
-                item2.setName("c");
-                item3.setName("z");
-                add(item);
-                add(item1);
-                add(item2);
-                add(item3);
+                add(new Item("a"));
+                add(new Item("j"));
+                add(new Item("c"));
+                add(new Item("z"));
             }
         };
         List<Item> expected = new ArrayList<>() {
             {
-                Item item = new Item();
-                Item item1 = new Item();
-                Item item2 = new Item();
-                Item item3 = new Item();
-                item.setName("z");
-                item1.setName("j");
-                item2.setName("c");
-                item3.setName("a");
-                add(item);
-                add(item1);
-                add(item2);
-                add(item3);
+                add(new Item("z"));
+                add(new Item("j"));
+                add(new Item("c"));
+                add(new Item("a"));
             }
         };
         Collections.sort(list, new ItemDescByName());
-        System.out.println(list);
-        System.out.println(expected);
         Assert.assertEquals(expected, list);
     }
 
@@ -54,39 +36,21 @@ public class ItemSortTest {
     public void sortAsc() {
         List<Item> list = new ArrayList<>() {
             {
-                Item item = new Item();
-                Item item1 = new Item();
-                Item item2 = new Item();
-                Item item3 = new Item();
-                item.setName("a");
-                item1.setName("z");
-                item2.setName("c");
-                item3.setName("f");
-                add(item);
-                add(item1);
-                add(item2);
-                add(item3);
+                add(new Item("a"));
+                add(new Item("z"));
+                add(new Item("c"));
+                add(new Item("f"));
             }
         };
         List<Item> expected = new ArrayList<>() {
             {
-                Item item = new Item();
-                Item item1 = new Item();
-                Item item2 = new Item();
-                Item item3 = new Item();
-                item.setName("a");
-                item1.setName("c");
-                item2.setName("f");
-                item3.setName("z");
-                add(item);
-                add(item1);
-                add(item2);
-                add(item3);
+                add(new Item("a"));
+                add(new Item("c"));
+                add(new Item("f"));
+                add(new Item("z"));
             }
         };
         Collections.sort(list, new ItemAscByName());
-        System.out.println(list);
-        System.out.println(expected);
         Assert.assertEquals(expected, list);
     }
 }

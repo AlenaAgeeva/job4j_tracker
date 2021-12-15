96ccb093e670a0bdace24c60b7a9c54be43c5167
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-15 14:22:01 -0400

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/tracker/function/SearchFolder.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.function;
+
+import java.util.*;
+import java.util.function.*;
+
+public class SearchFolder {
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+            if (pred.test(f)) {
+                rsl.add(f);
+            }
+        }
+        return rsl;
+    }
+}

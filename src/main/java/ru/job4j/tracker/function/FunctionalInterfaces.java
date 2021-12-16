b276c04ac8505f79d25c1fe9a4204307b3719363
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-16 14:28:34 -0400

    1.2. Функциональный интерфейс [#247497 #240411]

--- src/main/java/ru/job4j/tracker/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/tracker/function/FunctionalInterfaces.java
@@ -15,14 +15,13 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
         BiPredicate<Integer, String> biPred = (i, s) -> {
-            if (i % 2 == 0 || s.length() == 4) {
                 System.out.println("key: " + i + " value: " + s);
                 return true;
-            }
-            return false;
         };
         for (Integer i : map.keySet()) {
-            biPred.test(i, map.get(i));
+            if (i % 2 == 0 || map.get(i).length() == 4) {
+                biPred.test(i, map.get(i));
+            }
         }
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
         List<String> strings = sup.get();

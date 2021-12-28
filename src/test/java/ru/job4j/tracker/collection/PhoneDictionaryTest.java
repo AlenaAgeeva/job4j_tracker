f6b30c3ab1ef16d33e22f4ef851f2d754a060003
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-28 18:28:00 -0400

    2. Local-Variable Type Inference [#70626]

--- src/test/java/ru/job4j/tracker/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/tracker/collection/PhoneDictionaryTest.java
@@ -10,24 +10,24 @@ public class PhoneDictionaryTest {
 
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertEquals(persons.get(0).getSurname(), "Arsentev");
     }
 
     @Test
     public void whenReturnEmptyList() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         phones.add(
                 new Person("Semen", "Semenov", "765872", "Moscow")
         );
-        ArrayList<Person> persons = phones.find("Igor");
+        var persons = phones.find("Igor");
         assertEquals(persons.size(), 0);
     }
 }
--- src/test/java/ru/job4j/tracker/collection/PriorityQueueTest.java
+++ src/test/java/ru/job4j/tracker/collection/PriorityQueueTest.java
@@ -8,23 +8,23 @@ public class PriorityQueueTest {
 
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertEquals(result.getDesc(), "urgent");
     }
 
     @Test
     public void whenLowerPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
         queue.put(new Task("urgent middle", 2));
         queue.put(new Task("low middle", 4));
-        Task result = queue.getTasks().getLast();
+        var result = queue.getTasks().getLast();
         assertEquals(result.getDesc(), "low");
     }
 }

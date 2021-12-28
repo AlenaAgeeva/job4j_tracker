44d222dc7b07fc354f59a60a5f26a16a34e16276
AlenaAgeeva <alenapvageeva@gmail.com>
2021-12-28 17:56:31 -0400

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/tracker/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/tracker/collection/DepartmentsTest.java
@@ -2,40 +2,38 @@ package ru.job4j.tracker.collection;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class DepartmentsTest {
 
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         Assert.assertEquals(result, expect);
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         Assert.assertEquals(result, expect);
     }
 
     @Test
     public void whenMissed2Times() {
-        List<String> input = Arrays.asList("k1/sk1/ssk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> input = List.of("k1/sk1/ssk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk1");
         List<String> result = Departments.fillGaps(input);
         Assert.assertEquals(result, expect);
     }
 
     @Test
     public void whenNonChangeFirstDepartment() {
-        List<String> input = Arrays.asList("k2");
-        List<String> expect = Arrays.asList("k2");
+        List<String> input = List.of("k2");
+        List<String> expect = List.of("k2");
         List<String> result = Departments.fillGaps(input);
         Assert.assertEquals(result, expect);
     }

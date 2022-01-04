85aabd898f29a6d2ff5b4217e7de3a619a479561
AlenaAgeeva <alenapvageeva@gmail.com>
2022-01-04 19:26:51 -0400

    4. Training 0

---
+++ src/main/java/ru/job4j/tracker/training/Merge.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker.training;
+
+import java.util.stream.IntStream;
+
+/**
+ * This class represents a simple model of merging two sorted arrays into one
+ *
+ * @author Alena Ageeva
+ * @version 1.0
+ */
+public class Merge {
+    /**
+     * Method of sorting and merging two arrays
+     *
+     * @param left  first array
+     * @param right second array
+     * @return new array with results
+     */
+    public static int[] merge(int[] left, int[] right) {
+        return IntStream
+                .concat(IntStream.of(left), IntStream.of(right))
+                .sorted()
+                .toArray();
+    }
+}

1e1759b8923acad197b61baa72ad9e24e2db1795
Alena Ageeva <alenaageevapv@gmail.com>
2024-08-16 20:53:14 -0300

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/main/Item.java
+++ src/main/java/ru/job4j/tracker/main/Item.java
@@ -1,14 +1,16 @@
 package ru.job4j.tracker.main;
 
+import lombok.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
+import java.time.temporal.ChronoUnit;
 
+@Data
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm");
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created =  LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 
     public Item() {
     }
@@ -26,50 +28,5 @@ public class Item {
         this.id = id;
         this.name = name;
         this.created = created;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER) + '}';
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
     }
 }
